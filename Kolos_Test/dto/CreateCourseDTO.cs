6e2ffb7a9a45ae62d5fe57ab34857dc9279c97f5
darl1ng-design <adam.sp220@gmail.com>
2026-06-20 23:47:43 +0200

    feat: add dto classes for fetch and post

---
+++ Kolos_Test/dto/CreateCourseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Kolos_Test.dto;
+
+public class CreateCourseDTO
+{
+    public string professorsLastName { get; set; }
+    public string title { get; set; }
+    public int credits { get; set; }
+    public int semester { get; set; }
+    public List<int> students { get; set; }
+}
---
+++ Kolos_Test/dto/GetProfessorInfoDTO.cs
@@ -0,0 +1,36 @@
+﻿namespace Kolos_Test.dto;
+
+public class GetProfessorInfoDTO
+{
+    public int professorId { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string email { get; set; }
+    public string name { get; set; }
+    public List<CoursesDTO> courses { get; set; }
+}
+
+public class CoursesDTO
+{
+    public int courseId { get; set; }
+    public string title { get; set; }
+    public int credits { get; set; }
+    public int semester { get; set; }
+    public List<EnrollmentsDTO> enrollments { get; set; }
+}
+
+public class EnrollmentsDTO
+{
+    public double grade { get; set; }
+    public string status { get; set; }
+    public StudentDTO student { get; set; }
+}
+
+public class StudentDTO
+{
+    public int studentId { get; set; }
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string email { get; set; }
+    public int enrollmentYear { get; set; }
+}
